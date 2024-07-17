230048ecc696c7547307e13b9516997d704fd2dc
hselmangumussoy <153692178+hselmangumussoy@users.noreply.github.com>
2024-07-17 13:28:14 +0300

    first commit

---
+++ src/main/java/com/hsgumussoy/blogsiteproject/BlogSiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hsgumussoy.blogsiteproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSiteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSiteProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsgumussoy/blogsiteproject/BlogSiteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsgumussoy.blogsiteproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSiteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
