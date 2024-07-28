e7c51af68e7f976cb23c7c884fe9e200e9bcacf3
hselmangumussoy <153692178+hselmangumussoy@users.noreply.github.com>
2024-07-28 19:14:08 +0300

    efsane bir toDto yazdım . mükemmel

--- src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/ArticleMapper.java
+++ src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/ArticleMapper.java
@@ -3,6 +3,9 @@ package com.hsgumussoy.blogsiteproject.domain.platform.article.impl;
 import com.hsgumussoy.blogsiteproject.domain.auth.user.api.UserDto;
 import com.hsgumussoy.blogsiteproject.domain.platform.article.api.ArticleDto;
 import com.hsgumussoy.blogsiteproject.domain.platform.category.api.CategoryDto;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 public class ArticleMapper {
     public ArticleMapper() {
@@ -18,6 +21,32 @@ public class ArticleMapper {
                 .likeCount(article.getLikeCount())
                 .user(userDto)
                 .category(categoryDto)
+                .build();
+    }
+
+    public  static ArticleDto toDto(Article article, List<UserDto> userDtoList, List<CategoryDto> categoryDtoList){
+        UserDto user = StringUtils.hasLength(article.getUserId()) ?
+                userDtoList.stream()
+                        .filter(c -> c.getId().equals(article.getUserId()))
+                        .findFirst()
+                        .orElseThrow()
+                : null;
+        CategoryDto category =  StringUtils.hasLength(article.getCategoryId()) ?
+                categoryDtoList.stream()
+                        .filter(c-> c.getId().equals(article.getCategoryId()))
+                        .findFirst()
+                        .orElseThrow()
+                :null;
+
+        return ArticleDto.builder()
+                .id(article.getId())
+                .likeCount(article.getLikeCount())
+                .category(category)
+                .content(article.getContent())
+                .created(article.getCreated())
+                .modified(article.getModified())
+                .title(article.getTitle())
+                .user(user)
                 .build();
     }
 
