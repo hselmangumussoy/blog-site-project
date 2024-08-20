5bac78402e1000a63ed883182e7b10b8bdcdb98f
hselmangumussoy <153692178+hselmangumussoy@users.noreply.github.com>
2024-08-20 15:05:00 +0300

    basit değişiklikler

--- src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/articlecategory/ArticleCategory.java
+++ src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/articlecategory/ArticleCategory.java
@@ -1,7 +1,5 @@
 package com.hsgumussoy.blogsiteproject.domain.platform.article.impl.articlecategory;
 
-import com.hsgumussoy.blogsiteproject.domain.platform.article.impl.Article;
-import com.hsgumussoy.blogsiteproject.domain.platform.category.impl.Category;
 import com.hsgumussoy.blogsiteproject.library.rest.AbstractEntityForIndexing;
 import jakarta.persistence.Entity;
 import lombok.Getter;
