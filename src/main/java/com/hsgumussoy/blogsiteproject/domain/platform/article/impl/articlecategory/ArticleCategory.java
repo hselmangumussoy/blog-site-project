125968928b379ddd725cc4cc30bfb003811fc5b9
hselmangumussoy <153692178+hselmangumussoy@users.noreply.github.com>
2024-08-20 15:04:41 +0300

    articleCategory extend alındı ve fieldlar eklendi

--- src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/articlecategory/ArticleCategory.java
+++ src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/articlecategory/ArticleCategory.java
@@ -1,4 +1,16 @@
 package com.hsgumussoy.blogsiteproject.domain.platform.article.impl.articlecategory;
 
-public class ArticleCategory {
+import com.hsgumussoy.blogsiteproject.domain.platform.article.impl.Article;
+import com.hsgumussoy.blogsiteproject.domain.platform.category.impl.Category;
+import com.hsgumussoy.blogsiteproject.library.rest.AbstractEntityForIndexing;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class ArticleCategory extends AbstractEntityForIndexing {
+    private String articleId;
+    private String categoryId;
 }
