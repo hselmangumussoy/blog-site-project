069182620fb996abd5ee21e32e366f5cc6c7b307
hselmangumussoy <153692178+hselmangumussoy@users.noreply.github.com>
2024-08-21 18:33:35 +0300

    articleuser bire çok ilişki ve ara tabloya ihtiyaç yok

--- src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/articleuser/ArticleUserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hsgumussoy.blogsiteproject.domain.platform.article.impl.articleuser;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleUserRepository extends JpaRepository<ArticleUser, String> {
-}
