30cd85f065db5dcd90edb780b2741252967b2cbd
hselmangumussoy <153692178+hselmangumussoy@users.noreply.github.com>
2024-08-21 18:33:57 +0300

    gereksiz metodlar silindi

--- src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/ArticleRepository.java
+++ src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/ArticleRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, String> {
-    Optional<Article> findByUserIdAndCategoryId(String id, String id1);
 }
--- src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/articlecategory/ArticleCategoryRepository.java
+++ src/main/java/com/hsgumussoy/blogsiteproject/domain/platform/article/impl/articlecategory/ArticleCategoryRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ArticleCategoryRepository extends JpaRepository<ArticleCategory, String> {
-    Iterable<? extends ArticleCategory> findByCategoryIdIn(List<String> categoryIds);
-}
+public interface ArticleCategoryRepository extends JpaRepository<ArticleCategory, String> {}
